45b75f5d5391fc32ac2c3ce01c36d43157c809dc
Jennifer Chow <jennego@gmail.com>
2021-05-03 22:05:46 -0700

    add react spring and the very beginnings of project

---
+++ src/components/title-page.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const TitlePage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      <div>
+        <h1>Jennifer Chow</h1>
+        <h2>Web Developer</h2>
+      </div>
+    </div>
+  )
+}
+
+export default TitlePage
