89777ba34e37d45c1820613ad4c8770eadb16ae8
Jennifer Chow <jennego@gmail.com>
2021-05-29 20:25:34 -0700

    finally got react spring parallax working

--- src/components/title-page.js
+++ src/components/title-page.js
@@ -1,19 +1,23 @@
 import React from "react"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 const TitlePage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignContent: "center",
-      }}
-    >
-      <div>
-        <h1>Jennifer Chow</h1>
-        <h2>Web Developer</h2>
+    <ParallaxLayer className="title-page" offset={0} speed={2.5}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "70vh",
+        }}
+      >
+        <div>
+          <h1>Jennifer Chow</h1>
+          <h2>Web Developer</h2>
+        </div>
       </div>
-    </div>
+    </ParallaxLayer>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,68 @@
-import * as React from "react"
+import React, { useRef } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import TitlePage from "../components/title-page"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
-const IndexPage = () => (
-  <Layout>
-    <TitlePage />
-  </Layout>
-)
+const IndexPage = () => {
+  const ref = useRef()
+  return (
+    <Parallax pages={2} style={{ top: "0", left: "0" }}>
+      <ParallaxLayer
+        offset={0}
+        speed={0.5}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "lightblue",
+        }}
+      >
+        <p>Scroll down</p>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={0.9}
+        speed={1}
+        style={{ backgroundColor: "#ff6d6d" }}
+      >
+        <p>Scroll up</p>
+      </ParallaxLayer>
+    </Parallax>
+  )
+}
+
+// const IndexPage = () => (
+//   <Layout>
+//     <TitlePage />
+//     <Parallax>
+//       <ParallaxLayer>
+//         <p>Scroll down</p>
+//       </ParallaxLayer>
+
+//       <ParallaxLayer
+//         offset={1}
+//         speed={2}
+//         style={{ backgroundColor: "#ff6d6d" }}
+//       />
+
+//       <ParallaxLayer
+//         offset={1}
+//         speed={0.5}
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           color: "white",
+//         }}
+//       >
+//         <p>Scroll up</p>
+//       </ParallaxLayer>
+//     </Parallax>
+//   </Layout>
+// )
 
 export default IndexPage
