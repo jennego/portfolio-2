7a6d0e79e3d188657b31eee60ffe3536fdfecbc5
Jennifer Chow <jennego@gmail.com>
2021-07-14 00:04:14 -0700

    deleted the slider off page 2 dummy

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -14,7 +14,6 @@ const SecondPage = () => {
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
-      <ProjectSlider />
     </Layout>
   )
 }
